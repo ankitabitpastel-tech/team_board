dbd3ff314d2e15205e138f11013f2bb9a34b99af
Ankita Dey <ankitadeybeporoya@Ankitas-MacBook-Air.local>
2025-10-28 18:12:28 +0530

    updated

--- my_app/urls.py
+++ my_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('employee/create', views.employee_create, name='employee_create'),
     path('employees', views.employee_list, name='employee_list'),
     path('employee', views.employee_detail, name='employee_detail'),
-    path('employee/remove', views.employee_remove, name='employee_remove'),
+    # path('employee/remove', views.employee_remove, name='employee_remove'),
 
     path('project/create', views.create_project, name='create_project'),
     path('projects', views.project_list, name='project_list'),
